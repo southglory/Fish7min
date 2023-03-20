fc8337c423c9415e82ef26aef066d5f46426f191
YoungKwang Nam <coolbirds@naver.com>
2023-03-20 18:50:30 +0900

    Syncronize var

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FishNet.Object;
+using FishNet.Object.Synchronizing;
+
+
+public class PlayerHealth : NetworkBehaviour
+{
+    [SyncVar] public int health = 10;
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        if (!base.IsOwner)
+            GetComponent<PlayerHealth>().enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            UpdateHealth(this, -1);
+        }
+    }
+
+    [ServerRpc]
+    public void UpdateHealth(PlayerHealth script, int amountToChange)
+    {
+        script.health += amountToChange;
+    }
+}
--- README.md
+++ README.md
@@ -7,3 +7,4 @@ https://youtube.com/playlist?list=PLF6lFlLzb6CSuf4g8ZR1VRq-TAgC6AQWD
 1. Multiplay setting.  
 2. Change Color with RPC server.  
 3. Spawn&Despawn Object with server.  
+4. [SyncVar] health.  

7c4c0ab13ee01928d7cb5da2660ed7b880937a15
YoungKwang Nam <coolbirds@naver.com>
2023-03-21 15:12:20 +0900

    Broadcast chat(in trouble)

---
+++ Assets/Scripts/ChatBroadcastGuide.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using TMPro;
+using FishNet;
+using FishNet.Broadcast;
+using FishNet.Connection;
+
+//Made by Bobsi Unity - for youtube
+public class ChatBroadcastGuide : MonoBehaviour
+{
+    public Transform chatHolder;
+    public GameObject msgElement;
+    public TMP_InputField playerUsername, playerMessage;
+
+    private void OnEnable()
+    {
+        InstanceFinder.ClientManager.RegisterBroadcast<Message>(OnMessageRecieved);
+        InstanceFinder.ServerManager.RegisterBroadcast<Message>(OnClientMessageRecieved);
+    }
+    private void OnDisable()
+    {
+        InstanceFinder.ClientManager.UnregisterBroadcast<Message>(OnMessageRecieved);
+        InstanceFinder.ServerManager.UnregisterBroadcast<Message>(OnClientMessageRecieved);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SendMessage();
+        }
+    }
+
+    private void SendMessage()
+    {
+	    Message msg = new Message()
+        {
+            username = playerUsername.text,
+            message = playerMessage.text
+        };
+
+        playerMessage.text = "";
+
+        if (InstanceFinder.IsServer)
+            InstanceFinder.ServerManager.Broadcast(msg);
+        else if (InstanceFinder.IsClient)
+            InstanceFinder.ClientManager.Broadcast(msg);
+    }
+
+    private void OnMessageRecieved(Message msg)
+    {
+        GameObject finalMessage = Instantiate(msgElement, chatHolder);
+	    finalMessage.GetComponent<TextMeshProUGUI>().text = msg.username + ": " + msg.message;
+	    finalMessage.SetActive(true);
+    }
+
+    private void OnClientMessageRecieved(NetworkConnection networkConnection, Message msg)
+    {
+        InstanceFinder.ServerManager.Broadcast(msg);
+    }
+
+    public struct Message : IBroadcast
+    {
+        public string username;
+        public string message;
+    }
+}
--- README.md
+++ README.md
@@ -9,3 +9,4 @@ https://youtube.com/playlist?list=PLF6lFlLzb6CSuf4g8ZR1VRq-TAgC6AQWD
 3. Spawn&Despawn Object with server.  
 4. [SyncVar] health.  
 5. Broadcast cubePosition, and the cube is moved into the position by Client.  
+6. Broadcast chat(now error).  
